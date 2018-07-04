68a6c72a6b96bae92ee2fece8c1be7accbedbee5
teomarcdhio <mmarcolinishop@gmail.com>
2018-07-04 19:41:20 +0100

    New horrizontal style

--- routes/gallery.js
+++ routes/gallery.js
@@ -12,6 +12,14 @@ ObjectId = require('mongodb').ObjectID;
 router.route('/:gallery/:_id').get(async (req, res) => {
 
   try {
+    const firstColore = await Picture.findOne({"gallery": "Colore"}).sort({ _id: 1 }).limit(1);
+    const firstBandw = await Picture.findOne({"gallery": "B&W"}).sort({ _id: 1 }).limit(1);
+    const firstAudacity = await Picture.findOne({"gallery": "Audacity"}).sort({ _id: 1 }).limit(1);
+    const firstCooling = await Picture.findOne({"gallery": "Cooling"}).sort({ _id: 1 }).limit(1);
+    const firstKidz = await Picture.findOne({"gallery": "Kidz"}).sort({ _id: 1 }).limit(1);
+    const firstMeetmeat = await Picture.findOne({"gallery": "Meetmeat"}).sort({ _id: 1 }).limit(1);
+    const firstMuse = await Picture.findOne({"gallery": "Muse"}).sort({ _id: 1 }).limit(1);
+    const firstSquatinn = await Picture.findOne({"gallery": "Squatinn"}).sort({ _id: 1 }).limit(1);
     const gallery = await Picture.findById(req.params._id).and({ gallery: req.params.gallery });
     console.log('id: ', gallery._id);
     const nextImage = await Picture.findById({ $gt: new ObjectId(gallery._id) }).and({ gallery: req.params.gallery }).sort({ _id: 1 }).limit(1);
@@ -21,6 +29,14 @@ router.route('/:gallery/:_id').get(async (req, res) => {
     if (nextImage != null && prevImage === null) {
       console.log('render only nextImage');
       res.render('gallery.hbs',{
+        firstColore: firstColore._id,
+        firstBandw: firstBandw._id,
+        firstAudacity: firstAudacity._id,
+        firstCooling: firstCooling._id,
+        firstKidz: firstKidz._id,
+        firstMeetmeat: firstMeetmeat._id,
+        firstMuse: firstMuse._id,
+        firstSquatinn: firstSquatinn._id,
         pageTitle: gallery.gallery,
         galleryTitle: gallery.gallery,
         picName: gallery.name,
@@ -32,6 +48,14 @@ router.route('/:gallery/:_id').get(async (req, res) => {
     } else if (nextImage === null && prevImage != null) {
       console.log("render only prevImage");
       res.render('gallery.hbs',{
+        firstColore: firstColore._id,
+        firstBandw: firstBandw._id,
+        firstAudacity: firstAudacity._id,
+        firstCooling: firstCooling._id,
+        firstKidz: firstKidz._id,
+        firstMeetmeat: firstMeetmeat._id,
+        firstMuse: firstMuse._id,
+        firstSquatinn: firstSquatinn._id,
         pageTitle: gallery.gallery,
         galleryTitle: gallery.gallery,
         picName: gallery.name,
@@ -43,6 +67,14 @@ router.route('/:gallery/:_id').get(async (req, res) => {
     } else if ( nextImage && prevImage ) {
       console.log("render normally");
       res.render('gallery.hbs',{
+        firstColore: firstColore._id,
+        firstBandw: firstBandw._id,
+        firstAudacity: firstAudacity._id,
+        firstCooling: firstCooling._id,
+        firstKidz: firstKidz._id,
+        firstMeetmeat: firstMeetmeat._id,
+        firstMuse: firstMuse._id,
+        firstSquatinn: firstSquatinn._id,
         pageTitle: gallery.gallery,
         galleryTitle: gallery.gallery,
         picName: gallery.name,
@@ -54,6 +86,14 @@ router.route('/:gallery/:_id').get(async (req, res) => {
     } else {
       console.log("render without any option");
       res.render('gallery.hbs',{
+        firstColore: firstColore._id,
+        firstBandw: firstBandw._id,
+        firstAudacity: firstAudacity._id,
+        firstCooling: firstCooling._id,
+        firstKidz: firstKidz._id,
+        firstMeetmeat: firstMeetmeat._id,
+        firstMuse: firstMuse._id,
+        firstSquatinn: firstSquatinn._id,
         pageTitle: gallery.gallery,
         galleryTitle: gallery.gallery,
         picName: gallery.name,
